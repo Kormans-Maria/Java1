9451dec7f45fad8dd8a53393115d6e71e9d22b1d
Kormans-Maria <maria.it@icloud.com>
2021-12-12 12:42:23 +0300

    third task3

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,6 +1,11 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
+
+    public static void main(String[] args){
+
+    }
+
     public static int fiboNumber(int n){
         int sum = 0;
         for (int i = 1; i <= n; i++){
@@ -9,4 +14,9 @@ public class CyclesGoldenFibo {
         }
         return sum;
     }
+
+    public static boolean isGoldenTriangle(int a, int b, int c){
+        return true;
+    }
+
 }
