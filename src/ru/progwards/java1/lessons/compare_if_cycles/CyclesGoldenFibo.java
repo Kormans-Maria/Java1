9b69a36dead661eb1cd849933b03d7340c2e9621
Kormans-Maria <maria.it@icloud.com>
2021-12-12 12:54:50 +0300

    third task5

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -8,7 +8,7 @@ public class CyclesGoldenFibo {
 
     public static int fiboNumber(int n){
         int sum = 0;
-        for (int i = 1; i <= n; i++){
+        for (int i = 1; i < n; i++){
             sum = sum + i;
 
         }
