665c8e2878719904a9007233a4c55fae5e85d2d3
Kormans-Maria <maria.it@icloud.com>
2022-01-10 06:56:09 +0300

    third task6

---
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -0,0 +1,20 @@
+package ru.progwards.java1.lessons.classes;
+
+public class ComplexNum {
+    double a;
+    double b;
+    public ComplexNum(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public String toString(){
+        String s = this.a + "+" + this.b + "i";
+        return s;
+    }
+
+    public ComplexNum add(ComplexNum num){
+        return num;
+    }
+
+}
